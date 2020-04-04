e77fc40f81a9f8f4150550858f5ed2a376f4fe17
George Arnold <george.arnold.88@gmail.com>
2020-04-04 11:09:51 -0400

    comments

---
+++ problem-1.js
@@ -0,0 +1,6 @@
+//Problem 1: Counting Sheep
+
+// What is the input to the program?
+// What is the output of the program?
+// What is the input to each recursive call?
+// What is the output of each recursive call?
